543d9f6db350d4baa3dc415428b3bea4df0d12d4
andrew zimatskiy <zimazky@gmail.com>
2024-01-08 20:26:19 +0300

    Добавлено создание модели с индексным буфером. Создание быстрых буферов для статических данных.

--- src/model.hpp
+++ src/model.hpp
@@ -18,7 +18,12 @@ namespace world {
         static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
       };
 
-      Model(Device &device, const std::vector<Vertex> &vertices);
+      struct Builder {
+        std::vector<Vertex> vertices{};
+        std::vector<uint32_t> indices{};
+      };
+
+      Model(Device &device, const Model::Builder& builder);
       ~Model();
 
       Model(const Model &) = delete;
@@ -29,10 +34,17 @@ namespace world {
 
     private:
       void createVertexBuffers(const std::vector<Vertex> &vertices);
+      void createIndexBuffers(const std::vector<uint32_t> &indices);
 
       Device& device;
       VkBuffer vertexBuffer;
       VkDeviceMemory vertexBufferMemory;
       uint32_t vertexCount;
+
+      bool hasIndexBuffer = false;
+      VkBuffer indexBuffer;
+      VkDeviceMemory indexBufferMemory;
+      uint32_t indexCount;
+
   };
 }
